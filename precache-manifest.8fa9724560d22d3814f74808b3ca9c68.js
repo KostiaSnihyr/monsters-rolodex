9996edbe851e19856934085c0af1fd4453a8da9c
las vega <kosssm155@gmail.com>
2020-03-10 23:57:16 +0200

    Updates

---
+++ precache-manifest.8fa9724560d22d3814f74808b3ca9c68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25edabf922a308e2d9ffe877e6d5bc33",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "575f86b8ffcf9db98841",
+    "url": "/monsters-rolodex/static/css/main.b03375e9.chunk.css"
+  },
+  {
+    "revision": "7fe0845a347327439809",
+    "url": "/monsters-rolodex/static/js/2.80027728.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.80027728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575f86b8ffcf9db98841",
+    "url": "/monsters-rolodex/static/js/main.6c225369.chunk.js"
+  },
+  {
+    "revision": "0a7538a84e8ef0422b17",
+    "url": "/monsters-rolodex/static/js/runtime-main.984b980e.js"
+  }
+]);
